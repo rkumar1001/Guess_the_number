d4fd058d8e1c7bfb72425cdf410150b49998c6e7
Rajat <rajat.kumar.d1994@gmail.com>
2024-01-23 13:53:31 -0800

    Initial commit

---
+++ app/src/main/java/com/rk/guessthenumber/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.rk.guessthenumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textView;
+
+    Animation animationImage, animationText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        animationImage = AnimationUtils.loadAnimation(this,R.anim.image_animation);
+        animationText = AnimationUtils.loadAnimation(this,R.anim.text_animation);
+
+        imageView.setAnimation(animationImage);
+        textView.setAnimation(animationText);
+
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity (new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+
+            }
+        }.start();
+    }
+}
